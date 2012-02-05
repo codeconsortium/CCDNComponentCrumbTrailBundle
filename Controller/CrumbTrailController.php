c7d4d0a4f7d6d9b5a799b75c812e5c148e1feec8
codeconsortium <reece@codeconsortium.com>
2012-02-05 02:18:47 +0000

    init commit

---
+++ Controller/CrumbTrailController.php
@@ -0,0 +1,78 @@
+<?php 
+
+/*
+ * This file is part of the CCDN CommonBundle
+ *
+ * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/> 
+ * 
+ * Available on github <http://www.github.com/codeconsortium/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CCDNComponent\CrumbTrailBundle\Controller;
+
+/**
+ * 
+ * @author Reece Fowell <reece@codeconsortium.com> 
+ * @version 1.0
+ */
+class CrumbTrailController
+{
+
+
+	/**
+	 * 
+	 * @access private
+	 */
+	private $crumbs = array();
+
+
+	/**
+	 *
+	 * @access public
+	 */
+	public function __construct()
+	{
+
+	}
+	
+	
+	/**
+	 * Implements the Fluent Interface
+	 *
+	 * @access public
+	 * @param $label, $url, $icon
+	 * @return $this
+	 */
+	public function add($label, $url, $icon = null)
+	{
+		$this->crumbs[] = array('label' => $label, 'url' => $url, 'icon' => $icon);
+		
+		return $this;
+	}
+
+
+	/**
+	 * 
+	 * @access public
+	 * @return Array() $crumbs
+	 */
+	public function getTrail()
+	{
+		return $this->crumbs;
+	}
+	
+	
+	/**
+	 *
+	 * @access public
+	 * @return int
+	 */
+	public function count()
+	{
+		return (count($this->crumbs) - 1);
+	}
+
+}
