93e434e3058cafc994b74883745dc7b85206640f
Reece Fowell <reece@codeconsortium.com>
2012-05-25 18:27:20 +0100

    Change crumb trail service name.

--- Component/Crumb.php
+++ Component/Crumb.php
@@ -11,14 +11,14 @@
  * file that was distributed with this source code.
  */
 
-namespace CCDNComponent\CrumbTrailBundle\Controller;
+namespace CCDNComponent\CrumbTrailBundle\Component;
 
 /**
  * 
  * @author Reece Fowell <reece@codeconsortium.com> 
  * @version 1.0
  */
-class CrumbTrailController
+class Crumb
 {
 
 
