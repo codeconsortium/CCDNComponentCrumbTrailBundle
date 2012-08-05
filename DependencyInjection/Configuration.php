3bc796a4ab702be11ced2a44d8f3f5e6d12f8bb9
Reece Fowell <reece@codeconsortium.com>
2012-08-05 05:56:13 +0100

    Update documentation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the CCDN CommonBundle
+ * This file is part of the CCDNComponent CrumbTrailBundle
  *
  * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/>
  *
@@ -13,8 +13,8 @@
 
 namespace CCDNComponent\CrumbTrailBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
 /**
  * This is the class that validates and merges configuration from your app/config files
