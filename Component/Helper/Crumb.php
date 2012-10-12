11c742d64b1cefb3af04416cb4fcb5540d46d693
Reece Fowell <reece@codeconsortium.com>
2012-10-12 13:06:26 +0100

    Re-organise directory structure for cleaner separation of components.

--- Component/Helper/Crumb.php
+++ Component/Helper/Crumb.php
@@ -11,7 +11,7 @@
  * file that was distributed with this source code.
  */
 
-namespace CCDNComponent\CrumbTrailBundle\Component;
+namespace CCDNComponent\CrumbTrailBundle\Component\Helper;
 
 /**
  *
